0c03abb6e74f58ff945e716f722bb673fd641a9a
RenattoGuzman <renttogs@gmail.com>
2025-10-29 20:35:07 -0600

    error fix v3

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from app.routers import industries, stocks, portfolio_with_risk, portfolio_max_s
 
 app = FastAPI(title="ProyectoGrad API")
 
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:5173", "https://v7vzxsv8-5173.use2.devtunnels.ms"],  # replace with specific origins in production, e.g. ["http://localhost:3000"]
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
 app.include_router(industries.router, tags=["industries"])
 app.include_router(stocks.router, tags=["stocks"])
 app.include_router(portfolio_with_risk.router, tags=["portfolio"])
@@ -13,11 +21,3 @@ app.include_router(portfolio_xtreme_variance.router, tags=["portfolio"])
 app.include_router(portfolio_risk_ranges.router, tags=["portfolio"])
 
 app.include_router(montecarlo.router, tags=["montecarlo"])
-
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["http://localhost:5173", "https://v7vzxsv8-5173.use2.devtunnels.ms"],  # replace with specific origins in production, e.g. ["http://localhost:3000"]
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
-)
